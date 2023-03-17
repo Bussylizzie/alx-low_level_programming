781c44aa2996f51fb86a7c2ad0af3d55d44bf7ec
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-16 17:02:41 -0700

    use puts to write out a sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * printing a sentence with puts
+ * puts writes the chatacters to specified standard output
+ *
+ * the sentence should end with new line
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	char my_char[] = "\"Programming is like building a multilingual puzzle";
+	       
+		puts(my_char);
+	return (0);
+}
+
