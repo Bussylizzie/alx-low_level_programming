03b6189be9739b761e984e540377db800daf6a9c
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-18 13:38:39 -0700

    base ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
