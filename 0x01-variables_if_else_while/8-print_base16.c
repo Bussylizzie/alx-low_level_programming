fd13d1633c3ded09a03307f502709fa29d7fa448
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-19 08:51:42 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	int b;
+
+	{
+		scanf("%x", &b);
+	putchar(b);
+	putchar('\n');
+	}
+	return (0);
+}
+
