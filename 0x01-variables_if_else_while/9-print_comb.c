1e274667a10cf831812ade678192cab1abf29ef7
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-20 00:58:31 -0700

    single digit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	int p;
+
+for (p = 0; p <= 9; ++p)
+{
+	putchar(p);
+}
+if (p != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+{
+	putchar('\n');
+return (0);
+}
+}
+
