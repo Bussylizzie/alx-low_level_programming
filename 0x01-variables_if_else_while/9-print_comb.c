09b500ba054c3ed5b2259979f04df0fc8914bddf
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-20 01:10:05 -0700

    single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int p;
 
-for (p = 0; p <= 9; ++p)
+for (p = '0'; p <= '9'; ++p)
 {
-	putchar('0' + p);
+	putchar(p);
 }
-if (p != 9)
+if (p != '9')
 {
 	putchar(',');
 	putchar(' ');
