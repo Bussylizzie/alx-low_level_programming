ae15adacef0e963dbfddc8d9083a4de6dd85c5a7
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-20 03:50:19 -0700

    102 task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,8 +15,10 @@ int main(void)
 		{
 			if (a != b && a < b)
 			{
+				putchar((a / 10) + '0');
 				putchar((a % 10) + '0');
 				putchar(' ');
+				putchar((b / 10) + '0');
 				putchar((b % 10) + '0');
 				if (a == 98 && b == 99)
 				continue;
