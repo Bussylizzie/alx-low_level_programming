b21f77c5bb04062913acf8d8ce3821c1c4befd8f
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-20 01:33:45 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '8'; ++i)
+	{
+		for (j = '1'; j <= '9'; ++j)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+		}
+	}
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
+
