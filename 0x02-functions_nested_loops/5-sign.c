1440985aeae8e6629ed9a5ad5d5126192319c1e3
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-21 16:30:55 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - display signs of a number
  * @n: character to be checked
  *
- * Return: 1 for positive and greater than, 0 for zero, -1 for negative and lower than zero
+ * Return: 1 for positive, 0 for zero, -1 for negative
  *
  */
 int print_sign(int n)
@@ -19,7 +19,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
