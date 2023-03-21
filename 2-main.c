2553e9f461c258a3cb2b0b95643ed8926afe549e
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-21 05:43:01 -0700

    header file

---
+++ 2-main.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @_putchar: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+		return (0)
+}
+
