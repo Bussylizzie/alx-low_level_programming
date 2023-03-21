cce040dbdce0ecc58bb8190291f245525782d8bb
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-21 07:52:11 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - display alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char p;
+
+	for (p = 'a'; p <= 'z'; ++p)
+	{
+		_putchar('p');
+		_putchar('\n');
+}
+}
