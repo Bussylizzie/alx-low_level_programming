df346b66b92b4dfad6b36154bea2c70fa58efe92
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-21 16:05:59 -0700

    lowercase and uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isalpha - display letter whether in lowercase or uppercase
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 if lowercase or uppercase, 0, otherwise.
+ */
+int _isalpha(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
