d2ed008811c666b960dfa0aac63cb750640cb707
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-22 11:50:03 -0700

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - add integers together
+ *
+ * @b: integer to add
+ * @a: integer to add
+ * Return: Always 0.
+ */
+int add(int a, int b)
+
+{
+	int int_add;
+	int_add = a + b
+	{
+		_putchar(int_add);
+	}
+	return (0);
+}
+
+
