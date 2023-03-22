47226b8e3a85ad0652a9fe5573ba3bccd3e4af09
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-22 01:13:58 -0700

    absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - display the absolute number of an integer
+ *@a: the integer to be checked
+ * Return: Always 0.
+ */
+int _abs(int a)
+{
+	while (a >= 0)
+	{
+		if (a < 0)
+			a = a * -1;
+	}
+	return (0);
+}
+
+
