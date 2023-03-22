c73e073cdeef653aea20dc117b9d4b08df2875ef
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-22 05:25:01 -0700

    tast 7 last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,13 @@ int print_last_digit(int a)
 	int last_digit;
 
 	last_digit = a % 10;
-	if (a < 10)
-{
-	a = a * -1;
-}
-{
-	_putchar(last_digit + '0');
-}
-return (last_digit);
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	{
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 }
 
