f54026c70e3dc2a9cb569a8ab8620617ff943ad9
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-22 09:00:53 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,21 +7,22 @@
 void times_table(void)
 
 {
-	int i, j, k, p;
+	int i = '0';
+	int j, k, l;
 
-	i = '0';
-
-		while (i < 11)
+	while (i == '0')
+	{
+		for (j = '0'; j <= '9'; ++j)
 		{
-			for (j = 0; j <= 9; ++j)
+			for (k = '0'; k <= '9'; ++k)
 			{
-				for (k = 0; k <= 9; ++k)
-				{
-					p = j * k;
-						_putchar(p + '0');
-				_putchar('\n');
+				l = j * k;
 			}
+			_putchar(l);
+			_putchar('\n');
 		}
+	}
 }
-}
+
+
 
