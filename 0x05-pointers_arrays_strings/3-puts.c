c871df62f2aaa5369d7d4d0a454759bb9dc74ab2
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-27 09:31:08 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string
+ * @str: string to check
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
