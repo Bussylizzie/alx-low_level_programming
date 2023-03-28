dba2921e5afd378896249c60674aa6e58b39cad8
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-27 17:54:13 -0700

    4-print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,14 @@
 void print_rev(char *s)
 {
 	int count = 0;
+	int p;
 
 	while (*s != '\0')
 	{
 		++count;
 		++s;
 	}
-	for (count = count; count >= 0; --count)
+	for (p = count; p > 0; --p)
 	{
 		_putchar(*s);
 		--s;
