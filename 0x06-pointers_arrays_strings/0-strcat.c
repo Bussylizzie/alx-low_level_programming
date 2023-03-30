7634920fdd52436286d3529f60c29ff9635a2d21
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-30 09:26:56 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,14 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (dest[i])
+	{
+		i++;
+	}
 		for (j = 0; src[j] != '\0'; j++)
 		{
 			dest[i] = src[j];
+			i++;
 		}
 	dest[i] = '\0';
 	return (dest);
