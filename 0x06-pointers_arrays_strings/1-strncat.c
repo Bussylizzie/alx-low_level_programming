342e8a9f8985b4c8e02bb0a5db334277ab53ca92
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-30 11:00:20 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 	for (j = 0; src[j] != '\0'; j++)
-		while (src[j] && j < n)
+		while (src[j] && j <= n)
 			dest[i] = src[j];
 	dest[i] = '\0';
 	return (dest);
