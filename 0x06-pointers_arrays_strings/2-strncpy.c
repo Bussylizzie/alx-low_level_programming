7ad215ce31548d91c3dabbb44324a45052310d65
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-31 01:45:32 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,10 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 	while ((dest[i] = src[i]) != '\0' && i < n)
 	{
-		dest[i] = src[i];
 		i++;
 	}
-	while (i <= n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 	i++;
