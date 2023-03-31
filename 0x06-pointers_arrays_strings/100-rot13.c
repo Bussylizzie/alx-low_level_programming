721e5c719c6064a56b99716960493997dea6ab22
Bussylizzie <falaiyebusola2020@gmail.com>
2023-03-31 07:29:32 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rot13 - encodes a string using rot13
+ * @n: string pointer
+ *
+ * Return: return string
+ */
+char *rot13(char *n)
+{
+	char k[] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char p[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+	int i = 0;
+	int j;
+
+	while (n[i])
+	{
+		for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == k[j])
+			{
+				n[i] = p[j];
+			}
+		}
+	}
+		i++;
+	}
+	return (n);
+
+}
