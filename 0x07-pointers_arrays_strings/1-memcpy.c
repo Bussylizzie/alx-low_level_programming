c9a600242b59a249d40f7462f78d60afc3a02093
Bussylizzie <falaiyebusola2020@gmail.com>
2023-04-03 10:29:23 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	while (src[i] != '\0')
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++) /** i represents numbers of n bytes*/
+	       /** copied from memory area*/
 	{
 		dest[i] = src[i];
 	}
