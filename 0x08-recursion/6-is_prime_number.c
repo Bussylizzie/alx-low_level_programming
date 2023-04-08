5234be73a0a9b159348f471b9b1e3a67c29150e3
Bussylizzie <falaiyebusola2020@gmail.com>
2023-04-08 08:32:07 -0700

    6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - function that determine prime no
+ * @n: integer
+ * Return: return 1 for prime no or 0, otherwise
+ */
+int is_prime_number(int n)
+{
+	if ((n / 2 > 1) && (n / 3 > 1) && (n / 5 > 1))
+	{
+		return (0);
+	}
+	else if ((n / 2 < 1) && (n / 3 < 1) && (n / 5 < 1))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
