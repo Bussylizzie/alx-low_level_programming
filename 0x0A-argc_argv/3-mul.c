d9cb852806553f562fe93b29395d6e2d1eff737a
Bussylizzie <falaiyebusola2020@gmail.com>
2023-04-10 12:34:42 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int k;
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	if (argc == 3)
+	{
+		k = i * j;
+	}
+	else if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	printf("%d\n", k);
+	return (0);
+}
+
+
